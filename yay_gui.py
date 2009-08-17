66b7c2d41ca3fd29c79714ffbeef2a292351d65e
jacktasia <jacktasia@gmail.com>
2009-08-16 21:33:41 -0500

    better speed unit message

--- yay_gui.py
+++ yay_gui.py
@@ -14,6 +14,7 @@ import dircache
 import os
 import sys
 import pickle
+import math
 
 # used in reloadTime filter so only image files are "seen"/counted
 def img_only(f):
@@ -261,9 +262,11 @@ class YayGui(RunThread):
 		
 		viewMenu.add(self.menuMiniMode)
 		editMenu.add(menuItemSetSpeed)
+		editMenu.addSeparator()
 		editMenu.add(menuItemPrune)
 		fileMenu.add(menuItemChangeFolder)
-		fileMenu.add(menuItemReload)	
+		fileMenu.add(menuItemReload)
+		fileMenu.addSeparator()
 		fileMenu.add(menuItemQuit)
 		
 		menuBar.add(fileMenu)
@@ -367,12 +370,90 @@ class YayGui(RunThread):
 		
 	def showSpeedDialog(self,event): # bad name..should be setPauseLength or something
 		self.setSpeed()
+		
+	
+	def add_s(self,num,word):
+		num = math.floor(num)
+		if num == 1:
+			return str(num) + " " + word 
+		else:
+			return str(num) + " " + word + "s"
+			
+	def pretty_speed(self,s):
+		m = 0
+		h = 0
+
+		if s > 60:
+			m = s/60
+		else:
+			return self.add_s(s,'second')
+			
+		if m > 60:
+			h = m/60
+		else:
+			return self.add_s(m,'minute')
+			
+		return self.add_s(h,'hour')
 
 	def setSpeed(self):
+		options = ("Normal Change Speed",
+					"Advanced Change Speed",
+					"No Change")
+		x = swing.JOptionPane.showOptionDialog(
+					None,
+					"Speed is currently: " + self.pretty_speed(self.ticks),
+					"Slideshow Speed",
+					swing.JOptionPane.YES_NO_CANCEL_OPTION,
+					swing.JOptionPane.QUESTION_MESSAGE,
+					None,
+					options,
+					options[2])
+					
+		x = int(x)
+		
+		if x == 0:
+			self.setNormalSpeed()
+		elif x == 1:
+			self.setAdvancedSpeed()
+		# no changing...
+	
+	def setNormalSpeed(self):
+		opts = ("5 seconds","10 seconds","30 seconds","45 seconds",
+				"1 minute",
+				"5 minutes","10 minutes","15 minutes","30 minutes",
+				"45 minutes","1 hour","2 hours","4 hours","8 hours")
+		# above but in seconds for set_speed
+		opt_values = (5,10,30,45,
+					60,
+					300,600,900,1200,
+					2700,3600,7200,14400,28800)
+					
+		x = swing.JOptionPane.showInputDialog(
+					None,
+					"Stay on each image for:",
+					"Set Slideshow Speed Normal",
+					swing.JOptionPane.PLAIN_MESSAGE,
+					None,
+					opts,
+					opts[0])
+		# i am sure the following can be shorter
+		if x is not None and len(x) > 0:
+			count = 0
+			save = 0
+			for i in opts:
+				if x == i:
+					save = count
+					break
+				count += 1
+			if save != 0:
+				self.set_speed(opt_values[save])
+				
+	
+	def setAdvancedSpeed(self):
 		a = swing.JOptionPane.showInputDialog(
                     None,
-                   "How many seconds before changing background?",
-                   "Set Slideshow Speed",
+                   "Stay on each image for how many seconds?",
+                   "Set Slideshow Speed Advanced",
                     swing.JOptionPane.PLAIN_MESSAGE,
                     None,
                     None,
@@ -380,7 +461,11 @@ class YayGui(RunThread):
 		
 		if a is not None:
 			if a.isdigit():
-				self.set_speed(int(a))
+				if int(a) > 28800:
+					self.showDialogError("28800 seconds (8 hours) is max")
+					self.setSpeed()
+				else:	
+					self.set_speed(int(a))
 			else:
 				self.showDialogError("Not a number!")
 				self.setSpeed()
@@ -388,7 +473,7 @@ class YayGui(RunThread):
 		
 	def showDialogError(self,msg):
 		swing.JOptionPane.showMessageDialog(None,msg,
-			"Error: What the french, toast?",
+			"Error: Boo...",
 			swing.JOptionPane.ERROR_MESSAGE)
 
 	
