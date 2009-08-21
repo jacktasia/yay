9f2aa40bfa2e3af395bb6fee726a51d13d6d7f18
jacktasia <jacktasia@gmail.com>
2009-08-20 23:19:33 -0500

    made slightly better trayicon images
    cleaned up not needed files. .class/.pyc

--- tray.py
+++ tray.py
@@ -1,17 +1,15 @@
 import java.awt as awt
 import javax.swing as swing
 
+## Just testing...should be more generic (passed icon_file,title)
+
 def tryTray():
 	tray = awt.SystemTray.getSystemTray()
 	popup = awt.PopupMenu()
-	icon = awt.Toolkit.getDefaultToolkit().getImage("yay.png")
+	icon = awt.Toolkit.getDefaultToolkit().getImage("yay16.png") 
 
 	trayIcon = awt.TrayIcon(icon,"yay", popup)
 	tray.add(trayIcon)
-
-
-
-
 
 if __name__ == '__main__':
 	tryTray()
