b58ae8809a4db8dac06d9df255e1b08723261425
jacktasia <jacktasia@gmail.com>
2009-08-22 21:15:27 -0500

    made build_jar for faster runnable and added jythonlib.jar

--- build_jar.py
+++ build_jar.py
@@ -1,3 +1,17 @@
-## TODO
-a = subprocess.Popen('jar cfm ',shell=True)
 
+import subprocess
+
+JYTHON_PATH = '/home/v/jython2.5.0/'
+HOME_PATH = '/home/v/Desktop/yay/'
+## TODO
+def runit(cmd):
+	a = subprocess.Popen(cmd,shell=True)
+	print "ran: " + cmd
+
+
+if __name__ == '__main__':
+	runit('rm *.class')
+	runit("javac -classpath jythonlib.jar *.java")
+	runit("cp jythonlib.jar yay.jar")
+	runit("jar ufm yay.jar manifest.txt *.class *.py *.gif")
+	#maybe use API to upload directly to "Downloads" section
